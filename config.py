643d89f31a059a770599fce1ad7533ba353fa3c7
DharmX1 <101920887+DharmX1@users.noreply.github.com>
2025-07-08 07:02:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "8d0fe8b5ae8149455681681253b2ef17"
-api_hash = "21601817"
-bot_token = "7558666783:AAFNYl8qCYrxhNQHkGOodp4alrFvD3viGzU"
-auth_users = [6764661699]
+api_id = "8d0fe8b5ae8145681681253b2ef17"
+api_hash = "211817"
+bot_token = "75586783:AAFNYl8qCxhNQHkGOodp4alrFvD3viGzU"
+auth_users = [67646699]
